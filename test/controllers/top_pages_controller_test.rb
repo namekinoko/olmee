bda8f79894c2a48e61cef422a72288929a7d3f7d
namekinoko <namekinoko11@gmail.com>
2021-12-09 07:46:37 +0900

    トップページのデザイン（未完成）を作成

---
+++ test/controllers/top_pages_controller_test.rb
@@ -0,0 +1,10 @@
+require "test_helper"
+
+class TopPagesControllerTest < ActionDispatch::IntegrationTest
+  
+  test "should get index" do
+    get top_pages_index_url
+    assert_response :success
+  end
+
+end
