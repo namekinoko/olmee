23ac09f23dac51fb4242e91aaf22bb17fa90f017
namekinoko <namekinoko11@gmail.com>
2021-12-18 14:52:31 +0900

    編集機能の未完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
 
   def index
     redirect_to( new_user_path )
@@ -25,6 +27,21 @@ class UsersController < ApplicationController
     end
   end
 
+  # ユーザーのプロフィールページへ
+  def edit 
+    @user = User.find( params[:id] )
+  end
+
+  def update
+    @user = User.find( params[:id] )
+    if @user.update( user_params )
+      flash[:success] = "編集に成功しました"
+      redirect_to( @user )
+    else
+      render 'edit'
+    end
+  end
+
   private
     def user_params
       params.require( :user ).permit( 
@@ -34,6 +51,20 @@ class UsersController < ApplicationController
         :password,
         :password_confirmation
        )
+    end
+
+    # ログイン済みユーザーかどうか確認
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "ログインしてください"
+        redirect_to login_url
+      end
+    end
+
+    # 正しいユーザーかどうか確認
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless @user == current_user
     end
 
 end
