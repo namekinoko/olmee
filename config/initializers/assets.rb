849f66e2a2f69400273f1fa9574f96905fe61af0
namekinoko <namekinoko11@gmail.com>
2021-12-19 10:24:13 +0900

    サービスメインページの作成（内容未完成）

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -16,4 +16,5 @@ Rails.application.config.assets.precompile += %w(
   admin.css 
   users/new.css
   sessions/new.css
+  services/main.css
 )
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-
+  get '/main', to: 'services#main'
   resources :users
 end
