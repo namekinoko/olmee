227fe8271b7bfbf7a22000cf40559e1da06259aa
namekinoko <namekinoko11@gmail.com>
2022-01-15 17:01:10 +0900

    グループ検索機能の追加

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,7 +1,11 @@
 class ServicesController < ApplicationController
   before_action :logged_in_user, only: [:index]
   def index
-    @groups = Group.all
+    if params[:keyword] != nil
+     @groups = Group.where( prefecture: params[:keyword] )
+    else
+     @groups = Group.all.order( id: 'DESC' )
+    end
   end
 
   def show
