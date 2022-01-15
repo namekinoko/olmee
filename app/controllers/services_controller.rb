352a610c3d32a998a26fcd01031daf84bac586cd
namekinoko <namekinoko11@gmail.com>
2022-01-16 01:18:53 +0900

    軽微な修正

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -2,9 +2,9 @@ class ServicesController < ApplicationController
   before_action :logged_in_user, only: [:index]
   def index
     if params[:keyword] != nil
-     @groups = Group.where( prefecture: params[:keyword] )
+      @groups = Group.where( prefecture: params[:keyword] )
     else
-     @groups = Group.all.order( id: 'DESC' )
+      @groups = Group.all.order( id: 'DESC' )
     end
   end
 
