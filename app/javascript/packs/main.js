e1a6ae7925758e341c8203876764de4d72f53395
namekinoko <namekinoko11@gmail.com>
2022-01-16 18:26:53 +0900

    一部ページレスポンシブ化

--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -1,18 +1,39 @@
 document.addEventListener('turbolinks:load', () => {
+  // サイトを開いた時キャッチフレーズを出現させる関数
+  function LoadAnimation(){
+    $('#catchphrase').each(function(){
+    $(this).addClass('appeartext');
+    let animeText = $(this).text();
+    let animeTextBox = ""
+    animeText.split('').forEach( function( a,t ){
+    if( t < 10 ){
+      animeTextBox += '<span style="animation-delay:.' + t + 's;">' + a + '</span>'
+    }
+    else{
+      animeTextBox += '<span style="animation-delay:1.' + t + 's;">' + a + '</span>'
+    }
+    });
+    $(this).html( animeTextBox );
+    })
+  ;}
+  // 〜ここまでサイトを開いた時キャッチフレーズを出現させる関数〜
+
 // ハンバーガーメニュー
-$(function() {
-  $('.btn-gnav').on("click", function(){
+$( function() {
+  $('.btn-gnav').on( "click", function(){
     $(this).toggleClass('open');
-    $('#gnav').toggleClass('open');
+    $( '#gnav' ).toggleClass('open');
   });
-
 });
-
 // メニューをクリックされたら、非表示に
-$(function() {
-  $('.btn-gnav .gnav-menu li a').on("click", function(){
+$( function() {
+  $('.btn-gnav .gnav-menu li a').on( "click", function(){
      $('#gnav').removeClass('open');
   });
 });
 // 〜〜ここまでハンバーガーメニュー〜〜
+  $(window).on( 'load', function () {
+    LoadAnimation();
+    $(this).removeClass('appeartext');
+  });
 });
