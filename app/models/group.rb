d5a4bf96104040505a55920962f859e37bd7a2e7
namekinoko <namekinoko11@gmail.com>
2022-01-24 09:39:01 +0900

    group.rbにvalidationを追加

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,8 +3,21 @@ class Group < ApplicationRecord
   has_many :users, through: :group_users, dependent: :destroy
   has_many :chats, dependent: :destroy
 
+  # グループモデルのvalidation
+  validate :check_date
   validates :user_id, presence: true
-  validates :content, presence: true, length: { maximum: 240 }
+  validates :date, presence: true
+  validates :prefecture, presence: true, format: {with: /\A.{2,}[都道府県]$\z/}
+  validates :purpose, presence: true
+  validates :number, presence: true
+  validates :content, presence: false, length: { maximum: 240 }
   validates :prefecture, presence: true
 
+  private
+    #  入力された日付が今日以降のものかチェック
+    def check_date
+      if self.date < Date.today
+      errors.add(:date, "は今日以降のものを選択してください")
+      end
+    end
 end
