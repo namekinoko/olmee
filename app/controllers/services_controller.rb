f8b2c6528924b4864bdc94bf5aa96b010d7baad1
namekinoko <namekinoko11@gmail.com>
2022-01-24 18:55:04 +0900

    kaminariを用いたページネーションの実装

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,10 +1,11 @@
 class ServicesController < ApplicationController
   before_action :logged_in_user, only: [:index]
+  
   def index
     if params[:keyword] != nil
-      @groups = Group.where( prefecture: params[:keyword] )
+      @groups = Group.where( prefecture: params[:keyword] ).page( params[:page] ).per( 9 )
     else
-      @groups = Group.all.order( id: 'DESC' )
+      @groups = Group.all.page( params[:page] ).per( 9 ).order( id: 'DESC' )
     end
   end
 
