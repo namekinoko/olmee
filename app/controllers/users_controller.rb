5589581904dfabc3dc9b80f3427e1264901711fa
namekinoko <namekinoko11@gmail.com>
2022-01-25 11:52:42 +0900

    マイページのviewとスタイルの完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find( params[:id] )
+    @groups = current_user.groups.page( params[:page] ).per( 6 )
   end
 
   def new
